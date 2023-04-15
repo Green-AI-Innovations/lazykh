255e534ac58c5a814a4ee23dd54e010332f494c9
Wensley Podrono <wensley.podrono@gmail.com>
2023-04-15 18:54:01 +0200

    Raise NotImplemented error
    This file is not meant to be run for now. The code depends on scipy, which is not
    added to the pyproject.toml file. Also, the need for this file is not clear, since we
    might not implement a music feature. If you want to run this file, you'll need to
    add it with poetry as an dependency.

--- text_to_video/lazykh/musicAdder.py
+++ text_to_video/lazykh/musicAdder.py
@@ -1,3 +1,10 @@
+raise NotImplementedError(
+    "This file is not meant to be run for now. The code depends on scipy, which is not"
+    + " added to the pyproject.toml file. Also, the need for this file is not "
+    + "clear, since we might not implement a music feature. If you want to run this "
+    + "file, you'll need to install scipy and add it to the pyproject.toml file."
+)
+
 import scipy.io.wavfile
 import math
 import numpy as np
@@ -11,17 +18,23 @@ rate, voiceDataPre = scipy.io.wavfile.read(AUDIO_FILE_LOCATION)
 rate, musicDataPre = scipy.io.wavfile.read(MUSIC_FILE_LOCATION)
 
 voiceData = np.array(voiceDataPre)
-musicData = np.array(musicDataPre)[:,0]
+musicData = np.array(musicDataPre)[:, 0]
 
 VOICE_LEN = voiceData.shape[0]
 MUSIC_LEN = musicData.shape[0]
 addedMusicData = np.zeros(voiceData.shape)
-for ind in range(0,VOICE_LEN,MUSIC_LEN):
-    if ind+MUSIC_LEN >= VOICE_LEN:
-        addedMusicData[ind:VOICE_LEN] = voiceData[ind:VOICE_LEN]+musicData[0:VOICE_LEN-ind]*musicMulti
+for ind in range(0, VOICE_LEN, MUSIC_LEN):
+    if ind + MUSIC_LEN >= VOICE_LEN:
+        addedMusicData[ind:VOICE_LEN] = (
+            voiceData[ind:VOICE_LEN] + musicData[0 : VOICE_LEN - ind] * musicMulti
+        )
     else:
-        addedMusicData[ind:ind+MUSIC_LEN] = voiceData[ind:ind+MUSIC_LEN]+musicData*musicMulti
+        addedMusicData[ind : ind + MUSIC_LEN] = (
+            voiceData[ind : ind + MUSIC_LEN] + musicData * musicMulti
+        )
 
-addedMusicData = 32768*addedMusicData/np.amax(addedMusicData) # multiply by 2^32 because audio files just are that way sometimes
+addedMusicData = (
+    32768 * addedMusicData / np.amax(addedMusicData)
+)  # multiply by 2^32 because audio files just are that way sometimes
 finishedData = np.asarray(addedMusicData, dtype=np.int16)
-scipy.io.wavfile.write(OUTPUT_FILE_LOCATION,rate,finishedData)
+scipy.io.wavfile.write(OUTPUT_FILE_LOCATION, rate, finishedData)
