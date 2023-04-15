ef09d0303b2dd8fa3fcdba9f1ea73e17d6ca7df1
Wensley Podrono <wensley.podrono@gmail.com>
2023-04-15 19:03:31 +0200

    Format videoFinisher.py

--- text_to_video/lazykh/videoFinisher.py
+++ text_to_video/lazykh/videoFinisher.py
@@ -1,7 +1,9 @@
 import argparse
 import os.path
 import os
+import shutil
 import subprocess
+
 
 def emptyFolder(folder):
     for filename in os.listdir(folder):
@@ -12,24 +14,35 @@ def emptyFolder(folder):
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
         except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            print("Failed to delete %s. Reason: %s" % (file_path, e))
     try:
         os.rmdir(folder)
     except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (folder, e))
+        print("Failed to delete %s. Reason: %s" % (folder, e))
 
 
-parser = argparse.ArgumentParser(description='blah')
-parser.add_argument('--input_file', type=str,  help='the script')
-parser.add_argument('--keep_frames', type=str,  help='do you want to keep the thousands of frame still images, or delete them?')
+parser = argparse.ArgumentParser(description="blah")
+parser.add_argument("--input_file", type=str, help="the script")
+parser.add_argument(
+    "--keep_frames",
+    type=str,
+    help="do you want to keep the thousands of frame still images, or delete them?",
+)
 args = parser.parse_args()
 INPUT_FILE = args.input_file
 KEEP_FRAMES = args.keep_frames
 
 
-
-command = "ffmpeg -r 30 -f image2 -s 1920x1080 -i "+INPUT_FILE+"_frames/f%06d.png -i "+INPUT_FILE+".wav -vcodec libx264 -b 4M -c:a aac -strict -2 "+INPUT_FILE+"_final.mp4 "
+command = (
+    "ffmpeg -r 30 -f image2 -s 1920x1080 -i "
+    + INPUT_FILE
+    + "_frames/f%06d.png -i "
+    + INPUT_FILE
+    + ".wav -vcodec libx264 -b 4M -c:a aac -strict -2 "
+    + INPUT_FILE
+    + "_final.mp4 "
+)
 subprocess.call(command, shell=True)
 
 if KEEP_FRAMES == "F":
-    emptyFolder(INPUT_FILE+"_frames")
+    emptyFolder(INPUT_FILE + "_frames")
