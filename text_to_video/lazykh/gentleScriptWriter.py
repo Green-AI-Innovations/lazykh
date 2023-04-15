5ed948e7d867a809c87cd04ba4509367eea32235
Wensley Podrono <wensley.podrono@gmail.com>
2023-04-15 18:43:11 +0200

    Remove unused imports

--- text_to_video/lazykh/gentleScriptWriter.py
+++ text_to_video/lazykh/gentleScriptWriter.py
@@ -1,17 +1,16 @@
 import argparse
-import os.path
 from utils import removeTags
 
-parser = argparse.ArgumentParser(description='blah')
-parser.add_argument('--input_file', type=str,  help='the script')
+parser = argparse.ArgumentParser(description="blah")
+parser.add_argument("--input_file", type=str, help="the script")
 args = parser.parse_args()
 INPUT_FILE = args.input_file
 
-f = open(INPUT_FILE+".txt","r+")
+f = open(INPUT_FILE + ".txt", "r+")
 script = f.read()
 f.close()
 
-f = open(INPUT_FILE+"_g.txt","w+")
+f = open(INPUT_FILE + "_g.txt", "w+")
 f.write(removeTags(script))
 f.flush()
 f.close()
