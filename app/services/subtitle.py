c5e58148fcaac4edd88b5706a957f63df637993f
mohammed <moodanwer@gmail.com>
2023-06-05 14:41:48 +0200

    Fix subtitle to group 6 words at a time instead of having onw word that disappear fast

--- app/services/subtitle.py
+++ app/services/subtitle.py
@@ -2,20 +2,37 @@ import subprocess
 import json
 import os
 
-
 def generate_srt(json_data):
-    transcript = json_data["transcript"]
     words = json_data["words"]
+    grouped_words = group_words(words, 3)
 
     srt_output = ""
-    for i, word in enumerate(words, start=1):
-        start_time = int(word["start"] * 1000)
-        end_time = int(word["end"] * 1000)
+    for i, word_group in enumerate(grouped_words, start=1):
+        start_time = int(word_group[0]["start"] * 1000)
+        end_time = int(word_group[-1]["end"] * 1000)
+
         srt_output += f"{i}\n"
         srt_output += f"{format_time(start_time)} --> {format_time(end_time)}\n"
-        srt_output += f"{word['word']}\n\n"
+        srt_output += " ".join(word["word"] for word in word_group) + "\n\n"
 
     return srt_output
+
+
+def group_words(words, group_size):
+    grouped_words = []
+    current_group = []
+
+    for word in words:
+        current_group.append(word)
+
+        if len(current_group) == group_size:
+            grouped_words.append(current_group)
+            current_group = []
+
+    if current_group:
+        grouped_words.append(current_group)
+
+    return grouped_words
 
 
 def format_time(milliseconds):
@@ -25,6 +42,8 @@ def format_time(milliseconds):
     milliseconds = milliseconds % 1000
 
     return f"{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}"
+
+
 
 def create_video_with_subtitles(video_path, json_file_path, video_output_path, subtitle_path):
 
